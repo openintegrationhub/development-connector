6d9981c129acef086d933cced83e63918b46b808
jkopen <jknoop@wice.de>
2020-08-26 14:33:49 +0200

    Starting npm module if no custom ferryman is supplied

---
+++ start.sh
@@ -0,0 +1,9 @@
+
+FILE=/ferryman/runGlobal.js
+if [ -f "$FILE" ]; then
+    echo "Custom ferryman found starting...."
+    node ./ferryman/runGlobal.js
+else
+    echo "Custom ferryman not found using existing npm module."
+    node ./node_modules/@openintegrationhub/ferryman/runGlobal.js
+fi
